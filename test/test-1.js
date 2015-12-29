60336763907b1f6be51bf5cbeb5fae469bea0c15
Toan Nguyen Gia <toan.nguyen@2359media.com>
2015-12-29 12:15:36 +0700

    try test fail

--- test/test-1.js
+++ test/test-1.js
@@ -4,7 +4,7 @@ var sum = require('../solutions/question-1');
 describe('Question 1', function() {
   describe('sum', function () {
     it('should return the sum of arguments', function () {
-      assert.equal(10, sum(4, 6));
+      assert.equal(11, sum(4, 6));
       assert.equal(20, sum(10, 8, 2));
     });
   });
